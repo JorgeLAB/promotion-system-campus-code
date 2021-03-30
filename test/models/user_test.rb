5195d20270bbc37e534be59bcedcad4f6c94985c
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-03-30 12:40:55 -0300

    Adicionando validação customizada de email, verificar se o domínio é iugu.com.br. Realização de testes de validação e internacionalização de mensagem.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'attribute email cannot be incorrect' do
+    user = User.new(email: 'mclovin@gmail.com', password: '1234567', password_confirmation: '1234567')
+
+    refute user.valid?
+
+    assert_includes user.errors[:email], "possui domínio incorreto, utilize @iugu.com.br"
+  end
 end
