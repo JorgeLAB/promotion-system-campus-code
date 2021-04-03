cfb79cfb2c557c336c6d2f280d3b9cda0ca93318
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-03 18:04:57 -0300

    Adicionando tabela de promorion_approvals para a aprovação de uma promoção por outro usuário.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_025949) do
+ActiveRecord::Schema.define(version: 2021_04_03_122602) do
 
   create_table "coupons", force: :cascade do |t|
     t.string "code"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2021_03_27_025949) do
     t.index ["name"], name: "index_product_categories_on_name", unique: true
   end
 
+  create_table "promotion_approvals", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "promotion_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["promotion_id"], name: "index_promotion_approvals_on_promotion_id"
+    t.index ["user_id"], name: "index_promotion_approvals_on_user_id"
+  end
+
   create_table "promotions", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -40,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_03_27_025949) do
     t.date "expiration_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_promotions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +66,7 @@ ActiveRecord::Schema.define(version: 2021_03_27_025949) do
   end
 
   add_foreign_key "coupons", "promotions"
+  add_foreign_key "promotion_approvals", "promotions"
+  add_foreign_key "promotion_approvals", "users"
+  add_foreign_key "promotions", "users"
 end
