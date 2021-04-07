abd221df84b663538a79b95eaac1c7e4626857ee
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-07 00:16:12 -0300

    Adicionando e configurando o SimpleCov à aplicação.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter 'jobs'
   add_filter 'mailers'
+  add_filter 'channels'
 end
 
 require_relative "../config/environment"
@@ -17,15 +18,15 @@ class ActiveSupport::TestCase
 
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-​
+
   parallelize_setup do |worker|
     SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
   end
-​
+
   parallelize_teardown do
     SimpleCov.result
   end
-​
+
   Minitest.load_plugins
   Minitest::PrideIO.pride!
 end
