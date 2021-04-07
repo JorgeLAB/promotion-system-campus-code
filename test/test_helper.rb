632787e72c8b5aa4428fe5f5438fd649db6c905e
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-06 23:56:09 -0300

    Adicionando configurações do rubocop.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,31 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'jobs'
+  add_filter 'mailers'
+end
+
 require_relative "../config/environment"
 require "rails/test_help"
 
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f }
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  # fixtures :all
+class ActiveSupport::TestCase
+
   include Warden::Test::Helpers
 
-  Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f }
-  # Add more helper methods to be used by all tests here...
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+​
+  parallelize_setup do |worker|
+    SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+  end
+​
+  parallelize_teardown do
+    SimpleCov.result
+  end
+​
+  Minitest.load_plugins
+  Minitest::PrideIO.pride!
 end
