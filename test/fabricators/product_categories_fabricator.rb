8ea1d07d1d8628a840c7b8207e24f2928e6ef178
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-08 22:52:33 -0300

    Refatoração, tornando o atributo code único por meio do Faker.

--- test/fabricators/product_categories_fabricator.rb
+++ test/fabricators/product_categories_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:product_category) do
-  code { Faker::Commerce.department(max: 1).upcase }
+  code { Faker::Commerce.unique.department(max: 1).upcase }
   name { |attrs| "Promoção de #{attrs[:code].capitalize}" }
 end
 
