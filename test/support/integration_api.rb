134738f792b3d0becf61059217cfff72e91dc094
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-10 11:00:12 -0300

    Adicionando método support para tratar o response.body da api e convertê-lo em json.

---
+++ test/support/integration_api.rb
@@ -0,0 +1,9 @@
+module IntegrationApi
+  def body_json(symbolize_keys: false)
+    json = JSON.parse(response.body)
+    symbolize_keys ? json.deep_symbolize_keys : json
+  rescue
+    return {}
+  end
+end
+
